429b2de30e042a3708ae318e0ebea1f6b8d1513b
mspMMXX <platter.markus@gmail.com>
2025-01-09 07:54:53 +0100

    Paketstruktur

--- app/src/main/java/com/yama/orbitcare/features/login/SignInActivity.kt
+++ app/src/main/java/com/yama/orbitcare/features/login/SignInActivity.kt
@@ -1,4 +1,4 @@
-package com.yama.orbitcare
+package com.yama.orbitcare.features.login
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.yama.orbitcare.R
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/yama/orbitcare/features/login/SignUpOrganisationActivity.kt
+++ app/src/main/java/com/yama/orbitcare/features/login/SignUpOrganisationActivity.kt
@@ -1,4 +1,4 @@
-package com.yama.orbitcare
+package com.yama.orbitcare.features.login
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,12 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.yama.orbitcare.R
 
-class SignUpEmployeeActivity : AppCompatActivity() {
+class SignUpOrganisationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.signup_employee_activity)
+        setContentView(R.layout.signup_organisation_activity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,12 +21,12 @@ class SignUpEmployeeActivity : AppCompatActivity() {
         }
     }
 
-    // Button - öffnet die OrganisationRegActivity
-    fun organisationSignUpButton(view: View) {
-        val organisationActivity = Intent(this, SignUpOrganisationActivity::class.java)
-        startActivity(organisationActivity)
-    }
+    // Button - öffnet die MitarbeiterRegActivity
+    fun employeeSignUpButton(view: View) {
+        val employeeActivity = Intent(this, SignUpEmployeeActivity::class.java)
+        startActivity(employeeActivity)
 
+    }
     // Button - öffnet die AnmeldenActivity
     fun signInButton(view: View) {
         val signInActivity = Intent(this, SignInActivity::class.java)
