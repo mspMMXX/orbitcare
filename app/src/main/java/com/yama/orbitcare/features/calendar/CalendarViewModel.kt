0bc0dc1dea92df3c5081bf5fe0d36e2129d12445
oekoo <y.dykow22@gmail.com>
2025-01-14 15:26:19 +0100

    Create CalendarViewModel class

---
+++ app/src/main/java/com/yama/orbitcare/features/calendar/CalendarViewModel.kt
@@ -0,0 +1,9 @@
+package com.yama.orbitcare.features.calendar
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.yama.orbitcare.data.models.Event
+
+class CalendarViewModel {
+}
