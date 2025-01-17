aab13ef16cd6fc4050fa6a76abf1e09b22f3249e
oekoo <y.dykow22@gmail.com>
2025-01-17 17:28:14 +0100

    Add functionality to ClientActivity BottomNav

--- app/src/main/java/com/yama/orbitcare/features/client/ClientActivity.kt
+++ app/src/main/java/com/yama/orbitcare/features/client/ClientActivity.kt
@@ -1,13 +1,20 @@
 package com.yama.orbitcare.features.client
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.yama.orbitcare.R
+import com.yama.orbitcare.features.calendar.CalendarActivity
+import com.yama.orbitcare.features.employee.EmployeeActivity
 
 class ClientActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +24,39 @@ class ClientActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initializeViews()
+        setupBottomNavigation()
+    }
+
+    private fun initializeViews() {
+        bottomNavigation = findViewById(R.id.bottomNavigation)
+    }
+
+    private fun setupBottomNavigation() {
+        bottomNavigation.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_calendar -> {
+                    // Start ClientActivity
+                    startActivity(Intent(this, CalendarActivity::class.java))
+                    finish() // Close current Activity
+                    true
+                }
+                R.id.navigation_group -> {
+                    // Start EmployeeActivity
+                    startActivity(Intent(this, EmployeeActivity::class.java))
+                    finish() // Close current Activity
+                    true
+                }
+                R.id.navigation_person -> {
+                    // Already in ClientView
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Set active menu button
+        bottomNavigation.selectedItemId = R.id.navigation_calendar
     }
 }
