3c85367bc838f995e38a22c481ea95941315f4da
oekoo <y.dykow22@gmail.com>
2025-01-25 12:40:22 +0100

    Fix client and employee layout to match BottomNav throughout app

--- app/src/main/java/com/yama/orbitcare/features/employee/EmployeeActivity.kt
+++ app/src/main/java/com/yama/orbitcare/features/employee/EmployeeActivity.kt
@@ -41,11 +41,7 @@ class EmployeeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_employee)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         initializeViews()
         setupBottomNavigation()
         setEmployeeList()
